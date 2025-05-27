bf11f235b806254f7662ae974828b3ce60e68e65
Yashu-x <n.thashenu@gmail.com>
2025-05-27 12:00:08 +0530

    add

--- log-reg-Client/vite.config.js
+++ log-reg-Client/vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 4173,
+//     host: true
+//   }
+// })
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 4173,
-    host: true
+    proxy: {
+      '/api': {
+        target: 'http://backend-service:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
+
